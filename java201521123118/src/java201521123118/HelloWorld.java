22dde11cd47021a5cef862f1a126e84c87fbc227
NiuraY <qkciw40jbdb2771@126.com>
2021-03-13 17:05:45 +0800

    第一周实验

---
+++ java201521123118/src/java201521123118/HelloWorld.java
@@ -0,0 +1,18 @@
+package java201521123118;
+
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNextInt()) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            if(Math.abs(a)>1000)
+            	System.out.println("a<=1000");
+            else
+                System.out.println(a + b);
+        }
+        in.close();
+    }
+}
