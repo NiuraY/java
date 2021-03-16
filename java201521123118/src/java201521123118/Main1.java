c545f7f0bb12cb852e42a614385af54f829189d1
NiuraY <qkciw40jbdb2771@126.com>
2021-03-16 11:40:59 +0800

    xxx

--- java201521123118/src/java201521123118/Main1.java
+++ java201521123118/src/java201521123118/Main1.java
@@ -1,5 +1,26 @@
 package java201521123118;
-
+import java.util.Arrays;
+import java.util.Scanner;
 public class Main1 {
+	public static void main(String[] args){
+		Scanner sc =new Scanner(System.in);
+		String choice = sc.nextLine();
+		while(sc.hasNextLine()){
+		int x = choice2Int(choice);
+		switch (x) {
+		case 1:
+			
+			break;
 
+		default:
+			break;
+		}
+	}
+}
+	private static int choice2Int(String choice) {
+		if(choice.equals("fib"))
+		return 1;
+		if(choice.equals())
+		return 0;
+	}
 }
