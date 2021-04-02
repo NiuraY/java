61c736fa52362dad86335a7551d349085b19d4e9
NiuraY <qkciw40jbdb2771@126.com>
2021-04-02 22:36:27 +0800

    3.3

--- java1/src/java1/Main.java
+++ java1/src/java1/Main.java
@@ -1,57 +1,58 @@
 package java1;
+import java.util.Arrays;
 import java.util.Scanner;
-import java.lang.String;
+class Circle{
+	private int radius;
+	public Circle(int radius){
+		this.radius = radius;
+	}
+	public int getPerimeter(){
+		return 2*(int)(Math.PI)*radius;
+	}
+	public int getArea(){
+		return (int)(Math.PI*radius*radius);
+	}
+	public String toString(){
+		return String.format("Circle [radius =%d]",radius);
+	}
+}
+class Rectangle{
+	private int width;
+	private int length;
+	public Rectangle(int width,int length){
+		this.width = width;
+		this.length = length;
+	}
+	public int getPerimeter(){
+		return 2*(width + length);
+	}
+	public int getArea(){
+		return width*length;
+	}
+	public String toString(){
+		return String.format("Rectangle [width=%d,length=%d]",width,length);
+	}
+}
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-     Scanner sc=new Scanner(System.in);
-     int n=sc.nextInt();
-     String[] str=new String[n];
-     for(int i=0;i<n;i++){
-    	 str[i]=sc.next();
-     }
-     while(sc.hasNext()){
-    	 String choice=sc.next();
-    	 switch (choice) {
-		case "sort1":
-			String str2;
-			for(int i=0;i<n-1;i++){
-				for(int j=i+1;j<n;j++){
-				int x=Integer.parseInt(str[i].substring(6, 13));
-				int y=Integer.parseInt(str[j].substring(6, 13));
-				if(x>y){
-					str2=str[i];str[i]=str[j];str[j]=str2;
-				}
-					
-				}
-			}
-			for(int i=0;i<n;i++){
-			System.out.println(str[i].substring(6, 10)+"-"+str[i].substring(10, 12)+"-"+str[i].substring(12, 14));
-			}
-			break;
-		case "sort2":
-			String str3;
-			for(int i=0;i<n-1;i++){
-				for(int j=i+1;j<n;j++){
-				int x=Integer.parseInt(str[i].substring(6, 13));
-				int y=Integer.parseInt(str[j].substring(6, 13));
-				if(x>y){
-					str3=str[i];str[i]=str[j];str[j]=str3;
-				}
-					
-				}
-			}
-			for(int i=0;i<n;i++){
-				System.out.println(str[i]);
-			}
-			break;
-
-		default:
-			System.out.println("exit");
+		Scanner sc = new Scanner(System.in);
+		Rectangle[] re = new Rectangle[2];
+		Circle[] cc = new Circle[2];
+		re[0] = new Rectangle(sc.nextInt(),sc.nextInt());
+		re[1] = new Rectangle(sc.nextInt(),sc.nextInt());
+		cc[0] = new Circle(sc.nextInt());
+		cc[1] = new Circle(sc.nextInt());
+		int sum1 = 0;
+		int sum2 = 0;
+		for(int i = 0;i<2;i++){
+			sum1+=re[i].getPerimeter()+cc[i].getPerimeter();
+			sum2+=re[i].getArea()+cc[i].getArea();
 		}
-    	 
-     }
+		System.out.println(sum1);
+		System.out.println(sum2);
+		System.out.println(Arrays.deepToString(re));
+		System.out.println(Arrays.toString(cc));
 	}
 
 }
